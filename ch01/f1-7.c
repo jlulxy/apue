3b524b671d00de991a1e9f555a4c103dde6f1cb6
yvanli <lxy4200@163.com>
2020-03-24 22:16:32 +0800

    add signal int

--- ch01/f1-7.c
+++ ch01/f1-7.c
@@ -1,17 +1,19 @@
 #include "apue.h"
 #include <sys/wait.h>
 
+static void sig_int(int);
+
 int main(void) {
     char buf[MAXLINE];
     pid_t pid;
     int status;
-
-    printf("## ");
+    if (signal(SIGINT, sig_int) == SIG_ERR)
+      err_sys("signal error");
+    printf("%% ");
     while (fgets(buf, MAXLINE, stdin) != NULL) {
         if (buf[strlen(buf) - 1] == '\n') {
             buf[strlen(buf) - 1] = 0;
         }
-		printf("pid1=%d\n", getpid());
         if ((pid = fork()) < 0) {
             err_sys("fork error");
         } else if (pid == 0) {
@@ -25,22 +27,25 @@ int main(void) {
 			 *
 			 * So great exercise, 2019-06-30.
 			 */
-			printf("c_pid=%d\n", getpid());
             execlp(buf, buf, (char *)0);
             err_ret("couldn't, execute: %s", buf);
 			/* exit(127) or not matters! */
 			//exit(127);
         }
 
-		printf("pid2=%d\n", getpid());
         if ((pid = waitpid(pid, &status, 0)) < 0) {
             err_sys("waitpid error with pid=%d, status=%d\n", pid, status);
         } else {
-			printf("waitpid success with pid=%d, status=%d\n", pid, status);
 		}
-		printf("pid3=%d\n", getpid());
 
-        printf("## ");
+        printf("%% ");
     }
     exit(0);
 }
+
+void sig_int(int signo)
+{
+  printf("interrupt\n%% ");
+  exit(1);
+}
+
