73ed8e5c6ec4f256baca096c63d2cf7931bf1fa7
yvanli <lxy4200@163.com>
2020-04-04 16:30:56 +0800

    sysconf

--- ch02/f2-16.c
+++ ch02/f2-16.c
@@ -5,7 +5,7 @@
 #ifdef PATH_MAX
 static long pathmax = PATH_MAX;
 #else
-static ling pathmax = 0;
+static long pathmax = 0;
 #endif
 
 static long posix_version = 0;
@@ -13,46 +13,40 @@ static long xsi_version = 0;
 
 #define PATH_MAX_GUESS 1024
 
-char* path_alloc(size_t *sizep) {
-    char *ptr;
+char * path_alloc(size_t *sizep)
+{
+	char *ptr;
 	size_t size;
-	
-	if (posix_version == 0) {
+	if (posix_version == 0)
 		posix_version = sysconf(_SC_VERSION);
-	}
-	if (xsi_version == 0) {
+	
+	if (xsi_version == 0)
 		xsi_version = sysconf(_SC_XOPEN_VERSION);
-	}
+	
 	if (pathmax == 0) {
 		errno = 0;
-		if ((pathmax = pathconf("/", _PC_PATH_MAX)) < 0) {
-			if (errno == 0) {
-				pathmax = PATH_MAX_GUESS;
-			} else {
-				err_sys("pathconf error for _PC_PATH_MAX");
-			}
-		} else {
-			pathmax++;
+	}
+	if ((pathmax = pathconf("/",_PC_PATH_MAX) < 0 )){
+		if (errno == 0){
+			pathmax = PATH_MAX_GUESS;
+		}else{
+			err_sys("pathconf error for _PC_PATH_MAX");
 		}
+	} else {
+		pathmax++;
 	}
 
-	if ((posix_version < 200112L) && (xsi_version < 4)) {
+	if ((posix_version < 200112L) && xsi_version < 4){
 		size = pathmax + 1;
 	} else {
 		size = pathmax;
 	}
 
-	if ((ptr = malloc(size)) == NULL) {
+	if ((ptr = malloc(size)) == NULL){
 		err_sys("malloc error for pathname");
 	}
-
-	if (sizep != NULL) {
+	if (sizep != NULL){
 		*sizep = size;
 	}
-	return ptr;
+	return(ptr);
 }
-
-int main(void) {
-	printf("pathmax=%ld\n", pathmax);
-}
-
