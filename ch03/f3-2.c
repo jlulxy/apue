29a45dfbdd21dabcc16fa56f62733ec0b692d6d2
yvanli <lxy4200@163.com>
2020-04-05 14:10:47 +0800

    add hole file

--- ch03/f3-2.c
+++ ch03/f3-2.c
@@ -4,21 +4,25 @@
 char buf1[] = "abcdefghij";
 char buf2[] = "ABCDEFGHIJ";
 
-int main(void) {
+int main(void)
+{
     int fd;
-    if ((fd = creat("file.hole", FILE_MODE))  < 0) {
+    if ((fd = creat("file.hole",FILE_MODE)) <0){
         err_sys("creat error");
     }
-    if (write(fd, buf1, 10) != 10) {
-        err_sys("buf write error");
+
+    if(write(fd,buf1,10) != 10){
+        err_sys("buf1 write error");
     }
-    if (lseek(fd, 16384, SEEK_SET) == -1) {
+    /* offset now is 10 */
+    if (lseek(fd,16384,SEEK_SET) == -1){
         err_sys("lseek error");
     }
-    if (write(fd, buf2, 10) != 10) {
+    /* offset now = 16384 */
+    if (write(fd,buf2,10) != 10){
         err_sys("buf2 write error");
     }
 
+    /*offset now = 16394*/
     exit(0);
 }
-
